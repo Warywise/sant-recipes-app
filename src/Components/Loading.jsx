16528c8b9a45347865eab4d21e3ee504cf20ba2d
Gustavo Sant'Anna <g_santanna@outlook.com>
2021-12-13 15:57:02 -0300

    Cria Componente Loading

---
+++ src/Components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Loading() {
+  return (
+    <div className="loading">
+      <div className="dot-one" />
+      <div className="dot-two" />
+      <div className="dot-three" />
+    </div>
+  );
+}
